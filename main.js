a58067a872d611ddf821a2ed14598dce43d7dbc2
oRocha0 <105641851+oRocha0@users.noreply.github.com>
2025-08-01 19:26:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -53,13 +53,14 @@ senha = senha + alfabeto[numeroAleatorio];
 }
 campoSenha.value = senha;
 classificaSenha();
+classificaSenha();
 }function classificaSenha(){
 forcaSenha.classList.remove('fraca', 'media', 'forte');
 if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
 } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
 forcaSenha.classList.add('media');
-} else if (tamanhoSenha < 6 {
+} else if (tamanhoSenha < 5 {
 forcaSenha.classList.add('fraca');
 }
 }
