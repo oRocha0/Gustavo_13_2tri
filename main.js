c3eeed083e158dad5d276fab6811eca15ee5ef84
oRocha0 <105641851+oRocha0@users.noreply.github.com>
2025-08-08 19:01:05 -0300

    t

--- main.js
+++ main.js
@@ -52,15 +52,18 @@ numeroAleatorio = Math.floor(numeroAleatorio);
 senha = senha + alfabeto[numeroAleatorio];
 }
 campoSenha.value = senha;
-classificaSenha();
-classificaSenha();
-}function classificaSenha(){
+classificaSenha(alfabeto.length);
+}
+function classificaSenha(tamanhoAlfabeto){
+let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto);
 forcaSenha.classList.remove('fraca', 'media', 'forte');
-if (tamanhoSenha > 11){
+if (entropia > 57){
 forcaSenha.classList.add('forte');
-} else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+} else if (entropia > 35 && entropia < 57) {
 forcaSenha.classList.add('media');
-} else if (tamanhoSenha < 5) {
+} else if (entropia <= 35) {
 forcaSenha.classList.add('fraca');
 }
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6)*60*60*24;
 }
