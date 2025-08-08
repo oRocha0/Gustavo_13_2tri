d63f6baa24143b7673ccdff3ecb3708d3263d77b
oRocha0 <105641851+oRocha0@users.noreply.github.com>
2025-08-08 18:53:42 -0300

    ç

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
 } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
 forcaSenha.classList.add('media');
-} else if (tamanhoSenha < 5 {
+} else if (tamanhoSenha < 5) {
 forcaSenha.classList.add('fraca');
 }
 }
